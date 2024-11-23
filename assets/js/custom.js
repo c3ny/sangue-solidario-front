b6e01d718955b0103577956532c145027ecce00c
camaschi <114512680+camasch@users.noreply.github.com>
2024-11-23 19:08:01 -0300

    SSF-11-CORREÇÃO (#12)
    * Finalização da home
    
    Finalização home em bootstrap com os seguintes recursos implementados:
    - Mapa interativo com localização do user ou definição através de input de pesquisa;
    - Modal de login/cadastro;
    - Carousel de notícias do blog;
    
    * Criação de página de usuário, página de contato e alteração no script de mapa.
    
    * Correções pós merge
    
    * Páginas de perfil do hemocentros com barra de estoque, criação de solicitação e visualização.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,3 @@
-// Script para exibir campos dinamicamente com base no tipo de usuário selecionado
-document.getElementById('userType').addEventListener('change', function () {
-    const fisicaFields = document.getElementById('fisicaFields');
-    const juridicaFields = document.getElementById('juridicaFields');
-    const commonFields = document.getElementById('commonFields');
-    
-    if (this.value === 'fisica') {
-        fisicaFields.classList.remove('d-none');
-        juridicaFields.classList.add('d-none');
-    } else if (this.value === 'juridica') {
-        juridicaFields.classList.remove('d-none');
-        fisicaFields.classList.add('d-none');
-    }
-    commonFields.classList.remove('d-none');
-});
-
-
 // Função para rolar ao topo
 document.getElementById("backToTopBtn").addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
